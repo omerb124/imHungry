35af30e9336ab8a83b82c1777aeb5a756dcda98b
omerb304 <omerbachartadam@gmail.com>
2018-09-17 00:22:29 +0300

    .

--- vendor/composer/autoload_psr4.php
+++ vendor/composer/autoload_psr4.php
@@ -6,5 +6,6 @@ $vendorDir = dirname(dirname(__FILE__));
 $baseDir = dirname($vendorDir);
 
 return array(
+    'files' => array($vendorDir . '/simple-html-dom/simple-html-dom/simple_html_dom.php'),
     'Foodo\\' => array($baseDir . '/src'),
 );
